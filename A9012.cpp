771de6ee2514385ff9950b9d053c9d676fba4e03
han <dongh21021@gmail.com>
2018-07-03 14:51:48 +0900

    first commit

---
+++ A9012.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+bool isVps(const string& formula);
+int main(void) {
+	int n;
+	string formula;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> formula;
+		if (isVps(formula))
+			cout << "YES";
+		else
+			cout << "NO";
+	}
+}
+
+bool isVps(const string& formula) {
+	stack<char> openStack;
+
+	for (int i = 0; i < formula.size(); i++) {
+		if (formula[i] == '(')
+			openStack.push(formula[i]);
+		else if (formula[i] == ')') {
+			if (openStack.empty()) return false;
+			openStack.pop();
+		}
+	}
+	return openStack.empty();
+}
---
+++ brackets2.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+bool wellMatched(const string& formula);
+int main(void) {
+	int num;
+	string formula;
+
+	cin >> num;
+	for (int i = 0; i < num; i++) {
+		cin >> formula;
+		if (wellMatched(formula))
+			cout << "YES";
+		else
+			cout << "NO";
+	}
+}
+
+bool wellMatched(const string& formula) {
+	const string opening("([{"), closing(")]}");   //opening()????
+	stack<char> openStack;
+
+	for (int i = 0; i < formula.length(); i++) {
+		if (opening.find(formula[i]) != -1)   //여는 괄호라면
+			openStack.push(formula[i]);
+		else {  //여는 괄호가 아니라면
+			if (openStack.empty()) return false;
+			if (opening.find(openStack.top()) != closing.find(formula[i])) //짝이 안 맞으면
+				return false;
+			openStack.pop();
+		}
+	}
+	return openStack.empty();
+}
